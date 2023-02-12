2b12583e0bff198a8d332ec298db5d88f6a396d3
eelisl <eelloik@gmail.com>
2023-02-12 17:28:20 +0200

    Fix endif error

--- page.php
+++ page.php
@@ -13,21 +13,23 @@
  * @package air-light
  */
 
- // Featured image.
+// Featured image.
 
 
 
 get_header(); ?>
 
-<?php 
+<?php
 $featured_image = '';
 $image_position = '';
-if(get_field('kuvien_asettelu')):
+if (get_field('kuvien_asettelu')) :
 	$image_position = get_field('kuvien asettelu');
 endif;
 if (has_post_thumbnail()) :
 	$featured_image = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
-		<div class="entry-header-page" style="background-image: url(<?php echo $featured_image; ?>); background-position: 50% <?php if($image_position): echo $image_position; else: echo "50"?>%"></div>
+	<div class="entry-header-page" style="background-image: url(<?php echo $featured_image; ?>); background-position: 50% <?php if ($image_position) : echo $image_position;
+																															else : echo "50";
+																															endif; ?>%"></div>
 <?php endif; ?>
 
 <div id="content" class="content-area">
@@ -35,33 +37,34 @@ if (has_post_thumbnail()) :
 
 		<section id="page">
 			<div class="container">
-			
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<div class="col-md-8">
+
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<div class="col-md-8">
 							<h1><?php the_title(); ?></h1>
 							<?php the_content(); ?>
-					</div>
+						</div>
 
-					<div class="col-md-4">
+						<div class="col-md-4">
 							<?php get_sidebar(); ?>
-					</div>
-			<?php endwhile; endif; ?>
+						</div>
+				<?php endwhile;
+				endif; ?>
 			</div><!-- .container -->
 		</section>
 
-		<?php 
-		if( get_field('videon_kuva') && get_field('videon_url') ) {
-		?> 
-		<section id="yt-video" style="background-image: url(<?php echo the_field("videon_kuva"); ?>); padding: 150px 0; position: relative; background-size: cover;">
-			<div class="container">
-				<div class="play">
-					<a class="popup-youtube" href="<?php echo the_field("videon_url"); ?>">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/play.png"></a>
-				</div>
-				<div class="shade"></div>
+		<?php
+		if (get_field('videon_kuva') && get_field('videon_url')) {
+		?>
+			<section id="yt-video" style="background-image: url(<?php echo the_field("videon_kuva"); ?>); padding: 150px 0; position: relative; background-size: cover;">
+				<div class="container">
+					<div class="play">
+						<a class="popup-youtube" href="<?php echo the_field("videon_url"); ?>">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/play.png"></a>
+					</div>
+					<div class="shade"></div>
 
-			</div>
-		</section>
+				</div>
+			</section>
 
 		<?php } ?>
 
@@ -71,52 +74,51 @@ if (has_post_thumbnail()) :
 		<section id="blog">
 			<div class="container-larger">
 
-					<div class="row">
-							<h2>Viimeisimmät kirjoitukset</h2>
-		            <?php
-													$posts = 3;
-													$args = array(
-														'post_type' => 'post',
-														'posts_per_page' => $posts,
-													);
+				<div class="row">
+					<h2>Viimeisimmät kirjoitukset</h2>
+					<?php
+					$posts = 3;
+					$args = array(
+						'post_type' => 'post',
+						'posts_per_page' => $posts,
+					);
 
-													$post_query = new WP_Query($args);
+					$post_query = new WP_Query($args);
 
-													if ($post_query->have_posts()) {
-														while ($post_query->have_posts()) {
-															$post_query->the_post();
-															?>
-		                <div class="col-md-4">
-							<a href="<?php the_permalink(); ?>">
-								<div class="box">
-									<span class="date"><?php echo get_the_date(); ?></span>
-									<h3><?php the_title(); ?></h3>
-									<div class="lead">
-										<?php the_excerpt(); ?>
+					if ($post_query->have_posts()) {
+						while ($post_query->have_posts()) {
+							$post_query->the_post();
+					?>
+							<div class="col-md-4">
+								<a href="<?php the_permalink(); ?>">
+									<div class="box">
+										<span class="date"><?php echo get_the_date(); ?></span>
+										<h3><?php the_title(); ?></h3>
+										<div class="lead">
+											<?php the_excerpt(); ?>
+										</div>
 									</div>
-								</div>
-							</a>
+								</a>
 
-		                </div>
-		             <?php
+							</div>
+					<?php
 
-													}
-													wp_reset_postdata();
-												} else {
-													echo "<h3>Uusia julkaisuja odotellessa...</h3>";
-												}
-												?>
-		        </div>
-
-					<div class="show-all">
-							<a href="blogi-kannanotot">Näytä kaikki kirjoitukset <?php include get_theme_file_path('/svg/chevron-right.svg'); ?></a>
-					</div>
+						}
+						wp_reset_postdata();
+					} else {
+						echo "<h3>Uusia julkaisuja odotellessa...</h3>";
+					}
+					?>
 				</div>
-				</section>
 
-				<?php include("template-parts/social-media.php"); ?>
+				<div class="show-all">
+					<a href="blogi-kannanotot">Näytä kaikki kirjoitukset <?php include get_theme_file_path('/svg/chevron-right.svg'); ?></a>
+				</div>
+			</div>
+		</section>
+
+		<?php include("template-parts/social-media.php"); ?>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
 <?php get_footer();
-
