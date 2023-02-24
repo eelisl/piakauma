ceac25ffe9d094648bef0c5a0468c79d574ae295
eelisl <eelloik@gmail.com>
2023-02-24 19:55:51 +0200

    Social media feed changes

--- template-parts/social-media.php
+++ template-parts/social-media.php
@@ -13,17 +13,20 @@
 <!--- SOCIAL MEDIA FEED  -->
 <section id="feed">
     <div class="container-larger">
+        <h2>Pia sosiaalisessa mediassa</h2>
         <div class="col-md-12">
-            <h2>Pia sosiaalisessa mediassa</h2>
+            <h3>Twitter</h3>
             <?php echo do_shortcode("[custom-twitter-feeds feed=1]"); ?>
         </div>
 
     </div>
     <div class="container-larger">
-        <div class="col-md-6 col-sm-12">
+        <div class="col-md-5 col-sm-12">
+            <h3>Facebook</h3>
             <?php echo do_shortcode("[custom-facebook-feed feed=1]") ?>
         </div>
-        <div class="col-md-6 col-sm-12">
+        <div class="col-md-5 col-sm-12">
+            <h3>Instagram</h3>
             <?php echo do_shortcode("[instagram-feed feed=1]") ?>
         </div>
     </div>
